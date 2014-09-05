cdae2c733b3f05a2c1ba9908a554a18de150f1aa
ipip2005 <supersingerman@126.com>
2014-09-05 16:19:39 +0800

    布局修改，页面title更改

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,11 +1,11 @@
 @foreach($articles as $article)
 	<article class="article col-xs-12">
 		<header class="article-header row img-rounded bg-success">
-			<h1 class="article-title col-xs-12">
+			<h2 class="article-title col-xs-12">
 				<a href="/article/show?aid=<?php echo $article->id?>">
 					{{$article->title}}
 				</a>
-			</h1>
+			</h2>
 			
 			<div class="col-xs-6">
 				<span class="soft-text"><i class="icon-pencil"></i> {{$article->comment_count}} comments </span>
