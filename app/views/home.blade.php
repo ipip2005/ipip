6d93ca6d20e645ae3e1a85409966d0d0b7c5e7c5
ipip2005 <supersingerman@126.com>
2014-09-18 18:00:46 +0800

    search

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -11,11 +11,16 @@
 	</div>
 </div>
 <div class="col-xs-11 col-xs-offset-1 col-md-2 col-md-offset-0">
-    <div class="row">
-    	<aside class="col-xs-12">
-    		<script type="text/javascript">document.write(unescape('%3Cdiv id="bdcs"%3E%3C/div%3E%3Cscript charset="utf-8" src="http://rp.baidu.com/rp3w/3w.js?sid=9570093733661031698') + '&t=' + (Math.ceil(new Date()/3600000)) + unescape('"%3E%3C/script%3E'));</script>
-    	</aside>
-    </div>
+	{{ Form::open(['url' => 'search','method'=>'get']) }}
+	<div class="row">
+		<aside class="col-xs-12">
+			<div class="row">
+				{{ Form::text('search',Input::old('search'),['placeholder'=>'Search in blog...', 'class'=>'img-rounded col-xs-8']) }}
+				{{ Form::submit('Search',['class'=>'btn img-rounded col-xs-2']) }}
+			</div>
+		</aside>
+	</div>
+	{{ Form::close() }}
 	<div class="row">
 		<aside class="sidebar col-xs-12">
 		@include('sidebar/recentArticles')
@@ -26,6 +31,4 @@
 		@include('sidebar/highRateArticles')
 		</aside>
 	</div>
-	
-	
 </div>
