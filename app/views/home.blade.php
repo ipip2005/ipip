9d10397fc76f9a1b9c1970c29c052a0635343db4
ipip2005 <supersingerman@126.com>
2014-09-18 17:41:29 +0800

    百度搜索框

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -11,6 +11,11 @@
 	</div>
 </div>
 <div class="col-xs-11 col-xs-offset-1 col-md-2 col-md-offset-0">
+    <div class="row">
+    	<div aside class="col-xs-12">
+    		<script type="text/javascript">document.write(unescape('%3Cdiv id="bdcs"%3E%3C/div%3E%3Cscript charset="utf-8" src="http://rp.baidu.com/rp3w/3w.js?sid=9570093733661031698') + '&t=' + (Math.ceil(new Date()/3600000)) + unescape('"%3E%3C/script%3E'));</script>
+    	</div>
+    </div>
 	<div class="row">
 		<aside class="sidebar col-xs-12">
 		@include('sidebar/recentArticles')
