d20133f44c3e9a26bad1cacdffb020308b6a344e
ipip2005 <supersingerman@126.com>
2014-09-20 22:39:19 +0800

    mysqlcft

---
+++ app/database/migrations/2014_09_20_223056_add_search_to_article_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSearchToArticleTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('articles', function (Blueprint $table) {
+            $table->dropIndex('search');
+            DB::statement('ALTER TABLE articles ADD FULLTEXT search(title, content) WITH PARSER mysqlcft');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
