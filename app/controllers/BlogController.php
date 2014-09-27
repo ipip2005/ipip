fb23f4cd49fd3a0ae0eac49c3e6676d1e20ed01d
ipip2005 <supersingerman@126.com>
2014-09-27 23:29:16 +0800

    feed xml

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -35,7 +35,7 @@ class BlogController extends BaseController {
 		else
 			$articles = $articles->where('hidden', '=', 'false')->paginate(20); 
 		$articles->getFactory ()->setViewName ( 'pagination::slider-3' );
-		$this->layout->title = $label->label_name;
+		$this->layout->title = strip_tags($label->label_name);
 		$this->layout->main = View::make ( 'home' )->nest ( 'content', 'index', compact ( 'articles') );
 	}
 	/**
@@ -95,12 +95,73 @@ class BlogController extends BaseController {
 	    $this->layout->main = View::make('home')->nest('content', 'index', compact('articles'));
 	    
 	}
-	public function getRss(){
-		$articles = Article::where('hidden', '=', 'false');
-		$dom = new DOMDocument();
+	public function getFeed(){
+	    $articles = Article::where('hidden', '=', 'false')->orderBy('id','desc')->get();
+		$dom = new DOMDocument('1.0','utf-8');
+		$feed = $dom->createElement('feed');
+		$feed->setAttribute('xmlns', 'http://www.w3.org/2005/Atom');
+		$dom->appendChild($feed);
+		$title = $dom->createElement('title','ipip\'s blog,ipip 的个人博客-叶寥亮');
+		$title->setAttribute('type', 'text');
+		$link = $dom->createElement('link','http://ipipblog.net/');
+		$language = $dom->createElement('language','en-us');
+		$id = $dom->createElement('id','ipip2005');
+		$subtitle = $dom->createElement('subtitle','这是我业余时间用laravel框架搭建的一个分享我是生活、学习与工作的博客，并不一定每篇文章对你都是有趣的，但一定是和我零距离的事件');
+		$subtitle->setAttribute('type', 'text');
+		$updated = $dom->createElement('updated',$articles[0]->created_at);
+	
+		$feed->appendChild($title);
+		$feed->appendChild($subtitle);
+		$feed->appendChild($link);
+		$feed->appendChild($language);
+		$feed->appendChild($updated);
 		
-		$rss = $dom->createElement('rss');
-		$rss->setAttribute('version', '1.0');
+		$author = $dom->createElement('author');
+	    $name = $dom->createElement('name','ipip');
+	    $uri = $dom->createElement('uri','http://ipipblog.net');
+	    $author->appendChild($name);
+	    $author->appendChild($uri);
+	    $feed->appendChild($author);
+	    $generator = $dom->createElement('generator','http://ipipblog.net');
+	    $feed->appendChild($generator);
+	    
+	    foreach ($articles as $article){
+	        $entry = $dom->createElement('entry');
+	        $feed->appendChild($entry);
+	        $a_id = $dom->createElement('id','http://ipipblog.net/article/show?aid='.$article->id);
+	        $a_title = $dom->createElement('title',strip_tags($article->title));
+	        $a_link = $dom->createElement('link');
+	        $a_link->setAttribute('rel','alternate');
+	        $a_link->setAttribute('href', 'http://ipipblog.net/article/show?aid='.$article->id);
+	        
+	        $a_link2 = $dom->createElement('link');
+	        $a_link2->setAttribute('rel','alternate');
+	        $a_link2->setAttribute('type', 'text/html');
+	        $a_link2->setAttribute('href', 'http://ipipblog.net/article/show?aid='.$article->id);
+	        $content = $dom->createElement('content','<p>url:<a href="'.'http://ipipblog.net/article/show?aid='.$article->id.'" target="_blank">'.strip_tags($article->title).'</a></p>');
+	        $content->setAttribute('type', 'html');
+	          
+	        $published = $dom->createElement('published',$article->created_at);
+	        $updated = $dom->createElement('updated',$article->updated_at);
+	        $entry->appendChild($a_id);
+	        $entry->appendChild($a_title);
+	        $entry->appendChild($published);
+	        $entry->appendChild($updated);
+	        $entry->appendChild($a_link);
+	        $entry->appendChild($a_link2);
+	        $entry->appendChild($author->cloneNode(true));
+	        $entry->appendChild($content);
+	        
+	    }
+	    
+		$xml = new SimpleXMLElement($dom->saveXML());
+		return Response::make($xml->asXML())->header('Content-type','text/xml');
+	}
+	public function getRss(){
+		$articles = Article::where('hidden', '=', 'false')->get();
+		$dom = new DOMDocument('1.0','utf-8');
+		
+		$rss = $dom->createElement('feed');
 		$dom->appendChild($rss);
 		
 		$channel = $dom->createElement('channel');
@@ -118,10 +179,13 @@ class BlogController extends BaseController {
 		foreach ($articles as $article){
 			$item = $dom->createElement('item');
 			$channel->appendChild($item);
-			$a_title = $dom->createElement('title',$article->title);
+			$a_title = $dom->createElement('title',strip_tags($article->title));
 			$a_link = $dom->createElement('link','http://ipipblog.net/article/show?aid='.$article->id);
+			$item->appendChild($a_title);
+			$item->appendChild($a_link);
 		}
-		return Response::setContent($rss);
+		$xml = new SimpleXMLElement($dom->saveXML());
+		return Response::make($xml->asXML())->header('Content-type','text/xml');
 	}
 	public function getDeadLink(){
 		$file = asset('/deadlink.txt');
