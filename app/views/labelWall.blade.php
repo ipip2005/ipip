76f895ef77b006be1fd8cfd1ef609a49b0a69c5d
ipip2005 <supersingerman@126.com>
2014-10-21 00:00:05 +0800

    google adsense fix

--- app/views/labelWall.blade.php
+++ app/views/labelWall.blade.php
@@ -8,14 +8,25 @@
 	</div>
 	<div class="row margin-0">
 		<?php static $line_sum=0;?>
-		@foreach($labels as $label) <a
-		    rel="nofollow"
+		@foreach($labels as $label) <a rel="nofollow"
 			href="/article-at-label?label_id=<?php echo $label->id?>"
 			class="bigger text-center btn-lg btn-square z0 white-text
-			col-xs-<?php if ($line_sum==10 || $line_sum==9) {echo 12-$line_sum;$line_sum=0;} else
-				if ($line_sum==8){echo 2;$line_sum=$line_sum+2;}else{ 
-				$num=2+($label->article_count>=$ave);$line_sum=$line_sum+$num;echo $num;}?> not-break">
-			<b>{{$label->label_name.'('.$label->article_count.')';}}</b> </a>
+			col-xs-<?php
+
+if ($line_sum == 10 || $line_sum == 9) {
+    echo 12 - $line_sum;
+    $line_sum = 0;
+} else 
+    if ($line_sum == 8) {
+        echo 2;
+        $line_sum = $line_sum + 2;
+    } else {
+        $num = 2 + ($label->article_count >= $ave);
+        $line_sum = $line_sum + $num;
+        echo $num;
+    }
+?> not-break"> <b>{{$label->label_name.'('.$label->article_count.')';}}</b>
+		</a>
 		<script>
 		    function randomColor(){
 			    var rmin = parseInt("44", 16), rmax = parseInt('CC',16);
@@ -30,17 +41,21 @@
 		    }
 			$('a:last').css("background-color",randomColor());
 		</script>
-		<!-- #4BAFF1 #74C974 -->
 		@endforeach
 	</div>
 </div>
 @endif
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- ad1 -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:728px;height:90px"
-     data-ad-client="ca-pub-1111817816732851"
-     data-ad-slot="3960458927"></ins>
-<script>
+<br>
+<div class="col-xs-12">
+	<div class="row margin-0 text-center">
+		<script async
+			src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<!-- ad1 -->
+		<ins class="adsbygoogle"
+			style="display: inline-block; width: 651px; height: 80px"
+			data-ad-client="ca-pub-1111817816732851" data-ad-slot="3960458927"></ins>
+		<script>
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
+	</div>
+</div>
